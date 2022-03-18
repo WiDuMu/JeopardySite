55b6cdb7aeb8ee15357cf20bac33eebc50c68704
wdunc <wduncanmurphy@gmail.com>
2022-03-18 17:50:22 -0500

    This completely changes the way the rendering works in an effort to reduce redraws.

--- docs/Jeopardy.js
+++ docs/Jeopardy.js
@@ -1,5 +1,6 @@
 'use strict';
 const JeopardyURL = "https://jservice.io/api/" //API URL
+var find = function(name){return document.getElementById(name)}
 function getRandomInt(min, max) { // A function that returns a random integer
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -25,7 +26,7 @@ async function getCategoryQuestions() {
     while (categories.length < 5) { //A category can fail, so we may have to look up >5 categories.
         const category = await getCategory(); //Pulls a random category
         const questions = await get(`clues?category=${category[0]['id']}`); //Pulls the questions for a category.
-        const values = questions.map(question => question['value']) //Pulls the values of those questions.
+        const values = questions.map(question => question['value']); //Pulls the values of those questions.
         if (!values.includes(null)) {
             /*If a question was a daily double, it has no value in the DB, and I can't use it
             without a lot of extra logic, it's easier to just ignore categories with daily doubles.*/
@@ -41,24 +42,20 @@ async function getCategoryQuestions() {
                 category[i] = question;
                 /*Category[0] is the category info, 1-6 is the questions*/
             }
-            categories.push(category)//Adds the selected question to the category that is returned.
+            categories.push(category);//Adds the selected question to the category that is returned.
         }
     }
-    return categories
+    return categories;
 }
 function fillBoard(board,tableData) { //This function fills the table with the data.
-    clearBoard(board);
-    const categoryRow = document.createElement('tr');
-    categoryRow.setAttribute('class', 'questionRow');
+    const categoryRow = document.getElementById('categories');
     for (const category of tableData) { //Populates the category names in the table
         const categoryName = document.createElement('th'); //Heading row for the table
         categoryName.textContent = category[0]['title']; // Puts category name in the row
         categoryRow.appendChild(categoryName);
     }
-    board.appendChild(categoryRow);
     for (let i = 1; i < 6; i++) {//Iterates over rows in the table, adding the relevant questions
-        const row = document.createElement('tr'); // Creates a new row in the table for cuestions
-        row.id = i * 100; // The row should all have the same value, either 100-500
+        const row = document.getElementById(`${i * 100}`)
         for (const category in tableData) {
             const questionBox = document.createElement('td'); // New box in row
             if (tableData[category][i]['answeredCorrectly'] !== undefined) { // Checks what if this question was asked earlier.
@@ -69,11 +66,10 @@ function fillBoard(board,tableData) { //This function fills the table with the d
             else {
                 questionBox.textContent = row.id; // Sets the contents of the box to the questions value.
                 questionBox.id = category; //Stores the category that the question comes from for later
-                questionBox.onclick = function stupitity() {askQuestion(questionBox, board, tableData);} // I have know idea why I need to put my function in a function.
+                questionBox.onclick = () => {askQuestion(questionBox, tableData);} // I have know idea why I need to put my function in a function.
             }
             row.appendChild(questionBox);
         }
-        board.appendChild(row);
     }
 }
 function clearBoard(board) { // Clears the board with the execption of the Jeopardy! heading, used by other functions.
@@ -101,28 +97,58 @@ function checkAnswer(givenAnswer, correctAnswer) { //Checks if the answer is cor
     if (givenAnswerString == correctAnswerString) {return true;} // Checks whether the answer is correct
     else {return false;}
 }
-function askQuestion(box, board, tableData) { //This function changes the board to one with the just the question being asked, checks the answer, and then repaints the
+async function askQuestion(box, tableData) { //This function changes the board to one with the just the question being asked, checks the answer, and then repaints the
+    // Setting things that need to be done before we go back to the main board.
+    box.onclick = () => {}
+    // Getting elements on the page that we need for later.
+    const mainBoard = document.getElementById('mainBoard');
+    const questionBody = document.getElementById('askQuestion');
+    const questionText = document.getElementById('questionText');
+    const inputField = document.getElementById('answerInputField');
+    const inputButton = document.getElementById('answerInputButton');
+    const showCorrectAnswer = document.getElementById('correctAnswer');
+    const showGivenAnswer = document.getElementById('givenAnswer');
+    // Setting up variables used later
     const value = box.textContent; // The value of the question being asked.
-    const category = box.id;
-    const question = tableData[category][value / 100]; // Uses the value of the box clicked on to derive the question being asked.
-    const row = document.createElement('tr'); //Boilerplate row
-    const qBox = document.createElement('td'); //Boilerplate box
-    row.appendChild(qBox);
-    qBox.textContent = question['question'];
-    clearBoard(board);
-    board.appendChild(row);
-    qBox.onclick = function whyDoesThisNeedToBeAFunction(){
-        const answer = prompt(question['question']);
-        question['answeredCorrectly'] = checkAnswer(answer, question['answer']);
-        /*That the above variable exists shows that the answer has been used before, while the value tells you whether
-        the contestant answered correctly for redrawing the board.*/
-        fillBoard(board,tableData);
+    const category = box.id; // The id of a box matches up with the position of it's category in tableData[]
+    const question = tableData[await category][await value / 100]; // Uses the value of the box clicked on to derive the question being asked.
+    questionText.textContent = 'Question:\n'+question['question']; // Puts the question on screen.
+    mainBoard.hidden = true; // Hides the main board
+    questionBody.hidden = false; // Shows the question
+    inputButton.onclick = () => { //Once the answer has been submitted.
+        inputButton.hidden = inputField.hidden = true; // Hides the inputs
+        const givenAnswer = inputField.value;
+        const answerIsCorrect = checkAnswer(givenAnswer, question['answer']); // Checks the answer
+        questionBody.classList = box.classList = answerIsCorrect;
+        //The above colors the box on the main board, and the Question screen red or green based on whether answer was correct.
+        showCorrectAnswer.textContent = 'Correct answer:\n'+question['answer']; // Puts correct answer on the screen.
+        showCorrectAnswer.hidden = false;
+        if (!answerIsCorrect) {
+            showGivenAnswer.textContent = 'Your answer: '+givenAnswer; // Only show your answer if it didn't match correct.
+            const overrideButton = document.getElementById('overrideWasRight'); // Allows you to override the check if it didn't catch it.
+            overrideButton.hidden = showGivenAnswer.hidden = false;
+            overrideButton.onclick = () => {questionBody.classList = box.classList = overrideButton.hidden = true;}
+        }
+        const continueButton = document.getElementById('continueButton');
+        continueButton.hidden = false;
+        continueButton.onclick = () => {
+            questionBody.hidden = showCorrectAnswer.hidden = showGivenAnswer.hidden = continueButton.hidden = true;
+            mainBoard.hidden = inputButton.hidden = inputField.hidden = false;
+            inputField.value = ''; // Reseting this for later.
+            questionBody.removeAttribute('class');
+        }
     }
 }
 async function game() { //Wraps ansync functions
-    const board = document.getElementById('board');
     const tableData = await getCategoryQuestions();
+    const startButton = document.getElementById('startButton');
+    fillBoard(board, tableData);
     console.log(tableData);
-    await fillBoard(board, tableData);
+    startButton.onclick = () => {
+        const startScreen = document.getElementById('startScreen');
+        const mainBoard = document.getElementById('mainBoard');
+        startScreen.hidden = true;
+        mainBoard.hidden = false;
+    }
 }
 game();
